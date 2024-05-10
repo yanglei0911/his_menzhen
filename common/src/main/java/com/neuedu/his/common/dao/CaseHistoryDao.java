155ce796c6d99a05337ebe18c21a3280cb269532
leiyangal <1273135128@qq.com>
2024-05-10 17:30:49 +0800

    删除模块

--- common/src/main/java/com/neuedu/his/common/dao/CaseHistoryDao.java
+++
@@ -1,6 +0,0 @@
-package com.neuedu.his.common.dao;
-
-import com.neuedu.his.common.bean.CaseHistory;
-
-public interface CaseHistoryDao extends BaseDao<CaseHistory> {
-}
--- common/src/main/java/com/neuedu/his/common/service/CaseHistoryService.java
+++
@@ -1,9 +0,0 @@
-package com.neuedu.his.common.service;
-
-import com.neuedu.his.common.bean.CaseHistory;
-import com.neuedu.his.common.dao.CaseHistoryDao;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CaseHistoryService extends BaseService<CaseHistory,CaseHistoryDao> {
-}
